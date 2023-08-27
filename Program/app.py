beadce7d5e45e53e9e6430600b1bb04798f7f77a
TGR1621 <119565536+TGR1621@users.noreply.github.com>
2023-08-27 15:31:07 +0700

    Update app.py

--- Program/app.py
+++ Program/app.py
@@ -90,4 +90,4 @@ def predict():
                                rekomendasi=rekomendasi, hasil_input=request.form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000)
